0f7fc6bd1f41ece22c75b52ca463627867175e26
aifa21 <37891597+aifa21@users.noreply.github.com>
2020-08-07 11:34:59 +0600

    again added

---
+++ template-string.js
@@ -0,0 +1,20 @@
+const first="aifa";
+const last="faruque";
+const fullname=first+" "+last+" is good girl.";
+console.log(fullname);
+const fullname2=`${first} ${last} is a good girl`;
+console.log(fullname2);
+
+
+//......multiline.....
+
+const multiline="something happened\n"+
+"nothing happened\n"+
+"everything happened."
+console.log(multiline);
+
+// Alternative
+const multiline2=`something happened
+nothing happened
+everything happened`
+console.log(multiline2);
